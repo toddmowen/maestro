837b909ae8d9d380ef99f363ee7c51e83c54f170
Todd Owen <todd.owen@cba.com.au>
2015-04-08 13:42:34 +1000

    Add more tests for Guard.expandPaths, .expandTransferredPaths.

--- maestro-core/src/test/scala/au/com/cba/omnia/maestro/core/hdfs/GuardSpec.scala
+++ maestro-core/src/test/scala/au/com/cba/omnia/maestro/core/hdfs/GuardSpec.scala
@@ -7,16 +7,38 @@ class GuardSpec extends ThermometerSpec { def is = s2"""
 HDFS Guard properties
 =====================
 
-  glob expands to matching directories            $globIncludesDirectories
+  glob expands to matching dirs                   $globIncludesDirectories
+  expandPaths skips processed dirs                $skipsProcessed
+  expandTransferredPaths skips uningested dirs    $skipsUningested
 """
 
   def globIncludesDirectories = {
     withEnvironment(path(getClass.getResource("/hdfs-guard").toString)) {
-      Guard.expandPaths("a*") must_== List(
+      Guard.expandPaths(s"$dir/user/a*") must_== List(
         s"file:$dir/user/a",
         s"file:$dir/user/a1"
         // excludes "a2" because it is a file
         // excludes "b" because it doesn't match the glob
+      )
+    }
+  }
+
+  def skipsProcessed = {
+    withEnvironment(path(getClass.getResource("/hdfs-guard").toString)) {
+      Guard.expandPaths(s"$dir/user/c*") must_== List(
+        s"file:$dir/user/c",
+        s"file:$dir/user/c_transferred"
+        // excludes "c_processed"
+      )
+    }
+  }
+
+  def skipsUningested = {
+    withEnvironment(path(getClass.getResource("/hdfs-guard").toString)) {
+      Guard.expandTransferredPaths(s"$dir/user/c*") must_== List(
+        s"file:$dir/user/c_transferred"
+        // excludes "c"
+        // excludes "c_processed"
       )
     }
   }
